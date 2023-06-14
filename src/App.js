a410a1c5652217326d80285d43a61c2b4d626ef5
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-06-13 22:19:31 -0500

    Set up link between welcome and login

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import {Amplify} from 'aws-amplify';
 import awsconfig from './aws-exports';
 import {Authenticator} from '@aws-amplify/ui-react'
 import '@aws-amplify/ui/dist/styles.css';
-
+import {Link} from 'react-router-dom'
 Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
-      <Authenticator />
       <title>TimeTracker</title>
       <h3>Welcome to Time Tracker</h3>
-      <a href="https://time-tracker.auth.us-east-2.amazoncognito.com/signup?client_id=pks8i30h65ogcqfvmr77745gp&response_type=code&redirect_uri=http://localhost:8000/logged_in.html">
-        Register or Login
-      </a>
+      <Link to="login">Login or Register</Link>
     </div>
   );
 }
 
-export default App;
+function Login(){
+  return(
+    <div className="App">
+      <title>Login</title>
+      <Authenticator/>
+      <h3>Hi!</h3>
+    </div>
+  )
+}
+
+export {App, Login};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App, Login} from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
