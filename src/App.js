7a436ff594d18dcc0d467035592b8b3f6d313ad0
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-06-13 21:14:07 -0500

    Switched to existing Cognito user pool
    Also made changes to App page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {Authenticator} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui/dist/styles.css';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <Authenticator />
       <title>TimeTracker</title>
       <h3>Welcome to Time Tracker</h3>
       <a href="https://time-tracker.auth.us-east-2.amazoncognito.com/signup?client_id=pks8i30h65ogcqfvmr77745gp&response_type=code&redirect_uri=http://localhost:8000/logged_in.html">
         Register or Login
       </a>
-    </>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    </div>
   );
 }
 
