c414f48136bad8b1278a82f3171efa4276604a39
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-06-21 12:44:04 -0500

    Connected request to input text fields

--- src/Content/Tracking.js
+++ src/Content/Tracking.js
@@ -7,41 +7,36 @@ async function logTime(username){
     const activity = document.getElementById('Activity').value;
     const start = document.getElementById('Start').value;
     const end = document.getElementById('End').value;
-    if(username == null || activity == null || start == null || end == null){
+    if(activity == "" || start == "" || end == ""){
         alert("You must fill in all text boxes.");
         return;
     }
-    console.log(activity);
     // instantiate a headers object
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
     // create a JSON object with parameters for API call and store in a variable
-    // var requestOptions = {
-    //     method: 'POST',
-    //     headers: myHeaders,
-    //     body: {
-    //         username: username,
-    //         activity: activity,
-    //         start: start,
-    //         end: end
-    //     },
-    // };
     var payload = JSON.stringify({
         "httpMethod": "PUT", 
-        "username": "Vinceyou1",
-        "activity": "coding",
-        "start": "17",
-        "end": "21"
-    })
+        "username": username,
+        "activity": activity,
+        "start": start,
+        "end": end
+    });
     var requestOptions = {
         method: 'PUT',
-        headers:{
-            "Content-Type": "application/json"
-        },
+        headers: myHeaders,
         body: payload
-      }
+    };
+    
+    // var requestOptions = {
+    //     method: 'PUT',
+    //     headers:{
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: payload
+    //   }
     // make API call with parameters and use promises to get response
     let response = await fetch("https://eeae2o3zd6.execute-api.us-east-2.amazonaws.com/default", requestOptions);
     let data = await response.json()
