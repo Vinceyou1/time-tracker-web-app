128f680f6dc5c312184bd796031bf1d2e531d391
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2023-07-01 19:05:16 -0500

    Changed API gateway to allow get function
    Integrated the getData function with dynamodb by passing in username and httpMethod as query parameters with the request URL

--- src/Content/Tracking.js
+++ src/Content/Tracking.js
@@ -13,7 +13,6 @@ import Stack from '@mui/material/Stack';
 let username = null;
 
 async function logActivity(startTime, endTime, activity){
-    console.log(activity);
     if(activity == "" || startTime.getTime() === 0 || endTime.getTime() === 0){
         alert("You must fill in all text boxes.");
         return;
@@ -45,17 +44,34 @@ async function logActivity(startTime, endTime, activity){
     }
 }
 
-const data = [
-    { activity: 'coding', start: '0', end: '1'},
-    { activity: 'coding', start: '5', end: '1'},
-    { activity: 'coding', start: '0', end: '1'},
-]
+async function getData(setData){
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    
+    // using built in JSON utility package turn object to string and store in a variable
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+    };
+
+    let response = await fetch("https://eeae2o3zd6.execute-api.us-east-2.amazonaws.com/default?httpMethod=GET&username="+String(username), requestOptions);
+    let json = await response.json();
+    let items = json.body.data;
+}
 
 function Tracking(){
     username = useLoaderData().toString();
     const [activity, setActivity] = React.useState("");
     const [startTime, setStartTime] = React.useState(new Date(0));
     const [endTime, setEndTime] = React.useState(new Date(0));
+    const [data, setData] = React.useState([
+        { activity: 'coding', start: '0', end: '1'},
+        { activity: 'coding', start: '5', end: '1'},
+        { activity: 'coding', start: '0', end: '1'},
+    ]);
     return (
         <div>
             <Stack className='Input' direction="row" spacing="1vw">
@@ -71,6 +87,7 @@ function Tracking(){
                     />
                 </LocalizationProvider>
                 <Button variant="contained" onClick={() => logActivity(startTime, endTime, activity)}>Log</Button>
+                <Button variant='contained' onClick={() => getData()}></Button>
             </Stack>
             {
                 data.map((activity) => <Activity name={activity.activity} start={activity.start} end={activity.end}/>)
